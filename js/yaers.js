f4a2ccadb1991aa0e376fce3fdf086e25254c1a5
leonardobussi <leonardobezerrabussy@gmail.com>
2021-02-03 22:45:35 -0300

    boa

---
+++ js/yaers.js
@@ -0,0 +1,15 @@
+var dia = 27
+var mes = 01
+var ano = 1998
+
+
+var date = new Date()
+var strin = ""+date
+var dataNOw = strin.slice(11,15)
+//console.log(dataNOw)
+
+
+
+function start(){
+    yaers.innerText=dataNOw;
+}
